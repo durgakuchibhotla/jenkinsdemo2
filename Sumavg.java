98677a3d02dfbc7c59c52b5d7a00e69f138ba2f3
Durgaaaaaaaaa <245122749009@mvsrec.edu.in>
2025-06-17 15:30:14 +0530

    Add files via upload

---
+++ Sumavg.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Sumavg {
+    public static void main(String args[]) {
+
+        int sum = 0;
+       
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        
+        System.out.println("The sum of first " + n + " numbers is " + sum);
+        System.out.println("The average of first " + n + " numbers is " + (sum /10));
+    }
+}
+
