d3af464d4307df327020852f3501a212be58ca64
durgakuchibhotla <245122749009@mvsrec.edu.in>
2025-07-24 19:42:29 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,11 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Durga Sreeya Kuchibhotla";
+        int rollNo = 24512274;
+        String department = "Computer Science Engineering";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Department: " + department);
+    }
+}
